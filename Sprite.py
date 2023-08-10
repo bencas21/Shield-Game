751f917bc214c73e8dec2b9812561b439a592d3c
Benjamin Cassatt <benjamin.cassatt@gmail.com>
2023-08-10 19:34:57 -0400

    Most Sprites animated

--- Sprite.py
+++ Sprite.py
@@ -1,10 +1,16 @@
-import pygame
+import threading
 
+import pygame
+import time
+from threading import Thread
 
 class sprite:
 
     def __init__(self, images, colorkey = None, size = (100,100)):
         self.animation_list = []
+
+        self.image_pointer = 0
+
         for image in images:
             animation_to_add = pygame.image.load(image).convert()
             animation_to_add = pygame.transform.scale(animation_to_add,size).convert()
@@ -12,6 +18,24 @@ class sprite:
 
             self.animation_list.append(animation_to_add)
 
-
     def get_animation_list(self):
         return self.animation_list
+
+    def next_image_pointer(self):
+        if self.image_pointer < len(self.animation_list) - 1:
+            self.image_pointer += 1
+        else:
+            self.image_pointer = 0
+
+
+
+    def get_image_pointer(self):
+        return self.image_pointer
+
+    def get_current_image(self):
+        return self.animation_list[self.image_pointer]
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pygame
+import random
+import threading
+import time
+
 from Sprite import sprite
 from Turning_Sprite import turning_sprite
 
@@ -8,14 +12,36 @@ screen = pygame.display.set_mode((1200, 720))
 clock = pygame.time.Clock()
 running = True
 
+# Creating Character and Shield Sprites
 CharacterAni = turning_sprite(['Images/CharacterFF.png', 'Images/CharacterFB.png',
-                               'Images/CharacterFL.png', 'Images/CharacterFR.png'],size=(75,112.5))
+                               'Images/CharacterFL.png', 'Images/CharacterFR.png'], size=(75, 112.5))
 ShieldAni = turning_sprite(['Images/Shield Down.png', 'Images/Shield Up.png',
-                               'Images/Shield Left.png', 'Images/Shield Right.png'])
-ShieldAni.left = pygame.transform.scale(ShieldAni.left, (30, 150))
-ShieldAni.right = pygame.transform.scale(ShieldAni.right, (30, 150))
-ShieldAni.forward = pygame.transform.scale(ShieldAni.forward, (150, 30))
-ShieldAni.backward = pygame.transform.scale(ShieldAni.backward, (150, 30))
+                            'Images/Shield Left.png', 'Images/Shield Right.png'])
+Fire = sprite(['Images/Fire1.png', 'Images/Fire2.png',
+               'Images/Fire3.png'], colorkey='black', size = (50,50))
+
+
+def animate_fire(fire_sprite):
+    while running:
+        fire_sprite.next_image_pointer()
+        time.sleep(0.5)  # Delay here for animation
+
+
+def animate_fire(fire_sprite):
+    while running:
+        fire_sprite.next_image_pointer()
+        time.sleep(0.1)  # Delay here for animation
+
+
+# Create and start the thread for Fire animation
+fire_animation_thread = threading.Thread(target=animate_fire, args=(Fire,))
+fire_animation_thread.start()
+
+# Reformating Shield (Will optimize later)
+ShieldAni.left = pygame.transform.scale(ShieldAni.left, (25, 125))
+ShieldAni.right = pygame.transform.scale(ShieldAni.right, (25, 125))
+ShieldAni.forward = pygame.transform.scale(ShieldAni.forward, (125, 25))
+ShieldAni.backward = pygame.transform.scale(ShieldAni.backward, (125, 25))
 
 while running:
     # poll for events
@@ -24,18 +50,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill('black')
+    screen.fill('Black')
 
     # RENDER YOUR GAME HERE
-    screen.blit(CharacterAni.get_current_image(), [screen.get_width()/2 - CharacterAni.get_current_image().get_width()/2
-        ,screen.get_height()/2- CharacterAni.get_current_image().get_height()/2])
+    screen.blit(CharacterAni.get_current_image(),
+                [screen.get_width() / 2 - CharacterAni.get_current_image().get_width() / 2
+                    , screen.get_height() / 2 - CharacterAni.get_current_image().get_height() / 2])
 
+    screen.blit(Fire.animation_list[Fire.get_image_pointer()], [50, 50])
+
+
+
+    if ShieldAni.determine_position() == 'forward':
+        x_sep_distance = 0
+        y_sep_distance = 75
+    elif ShieldAni.determine_position() == 'backward':
+        x_sep_distance = 0
+        y_sep_distance = -75
+    elif ShieldAni.determine_position() == 'left':
+        x_sep_distance = -75
+        y_sep_distance = 0
+    elif ShieldAni.determine_position() == 'right':
+        x_sep_distance = 75
+        y_sep_distance = 0
 
     screen.blit(ShieldAni.get_current_image(),
-                [screen.get_width() / 2 - ShieldAni.get_current_image().get_width() / 2
-                    , screen.get_height() / 2 - ShieldAni.get_current_image().get_height() / 2])
+                [screen.get_width() / 2 - ShieldAni.get_current_image().get_width() / 2 + x_sep_distance
+                    , screen.get_height() / 2 - ShieldAni.get_current_image().get_height() / 2 + y_sep_distance])
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
